0f1d9d3f28a29172562962c4525f0c05e6260ddd
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-04-01 23:01:13 -0300

    app: reset app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { Grid, Image, Heading, Text, Box, Button, Link, Stack } from '@chakra-ui/react'
-import { data } from './data'
+import { Grid, Text } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Grid w="100%" h="100vh" background={`url(${data.item.album.images[0].url})`} backgroundRepeat="no-repeat" backgroundPosition="center" backgroundSize="cover">
-      <Grid placeItems="center" w="100vw" h="100vh" sx={{ backdropFilter: 'blur(1.5rem)' }}>
-        <Stack alignItems="center" direction={['column', 'column', 'row']} spacing={8}>
-          <Image src={data.item.album.images[1].url} borderRadius="lg" boxShadow="2xl" />
-          <Box textAlign={["center", 'center', 'initial']}>
-            <Heading fontSize={['4xl', "6xl"]} color="white">{data.item.name}</Heading>
-            <Text fontSize="lg" color="white">from {data.item.artists[0].name}</Text>
-
-            <Button mt={4} as={Link} href={data.item.external_urls.spotify}>listen on spotify</Button>
-          </Box>
-        </Stack>
-      </Grid>
+    <Grid placeItems="center" w="100%" h="100vh" bgColor="green.50">
+      <Text color="green.400" fontWeight="bold">what i'm listening?</Text>
     </Grid>
   )
 }
